43c7ce5400d1472f571019dc4a56372b0ecac480
taesangeom <113084809+taesangeom@users.noreply.github.com>
2022-11-24 16:25:41 +0900

    filefunc.h 헤더파일, 이후 더 수정 가능

---
+++ filefunc.h
@@ -0,0 +1,26 @@
+#ifndef FILEFUNC_H
+# define FILEFUNC_H
+
+# include <fcntl.h>
+# include <ncurses.h>
+# include <curses.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+
+typedef struct Userdata {
+    char name[31];
+    int score;
+    int win;
+    int lose;
+} User;
+
+void userfilesave(char* loginname1, char* loginname2 int result1, int result2);
+
+void userfilecreate(char* newusername);
+
+#endif
