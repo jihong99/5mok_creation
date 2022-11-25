1dd978c0b1dff57f3f6c43ade11620582680a1ea
gwanhwi <rhksgnl3@naver.com>
2022-11-25 22:00:49 +0900

    feat : put baduk stone

--- 5mok/OmokUI.cpp
+++ 5mok/OmokUI.cpp
@@ -1,11 +1,12 @@
 #include <ncurses.h>
+#include <string.h>
 #include <vector>
-
 using namespace std;
 void run();
 void drawCheckerboard(WINDOW *win, vector<vector<char>> &checkerboard);
-void moveCursor(WINDOW *win, int &y, int &x);
-
+void moveCursor(WINDOW *win, int &y, int &x, vector<vector<char>> &checkerboard,
+                bool validate[][19], bool &player);
+void printCheckerboard(WINDOW *win, vector<vector<char>> checkerboard);
 int main(int argc, char const *argv[]) {
     run();
     return 0;
@@ -13,6 +14,11 @@ int main(int argc, char const *argv[]) {
 void run() {
     WINDOW *window1;
     vector<vector<char>> checkerboard;
+    bool validate[19][19];
+    for (int i = 0; i < 19; i++) {
+        memset(validate[i], true, sizeof(bool) * 19);
+    }
+    bool player = true; // true : player1, false : player2
     int x, y;
     initscr();
 
@@ -34,7 +40,7 @@ void run() {
     wmove(window1, 0, 0);
 
     while (1) { // test while
-        moveCursor(window1, y, x);
+        moveCursor(window1, y, x, checkerboard, validate, player);
     }
 
     getch();
@@ -59,6 +65,10 @@ void drawCheckerboard(WINDOW *win, vector<vector<char>> &checkerboard) {
     }
     checkerboard.push_back(row1);
 
+    printCheckerboard(win, checkerboard);
+}
+void printCheckerboard(WINDOW *win, vector<vector<char>> checkerboard) {
+    wclear(win);
     for (int i = 0; i < checkerboard.size(); i++) {
         for (int j = 0; j < checkerboard[i].size(); j++) {
             wprintw(win, "%c", checkerboard[i][j]);
@@ -67,7 +77,8 @@ void drawCheckerboard(WINDOW *win, vector<vector<char>> &checkerboard) {
     }
     wrefresh(win);
 }
-void moveCursor(WINDOW *win, int &y, int &x) {
+void moveCursor(WINDOW *win, int &y, int &x, vector<vector<char>> &checkerboard,
+                bool validate[][19], bool &player) {
     int c;
     keypad(win, TRUE);
     noecho();
@@ -76,28 +87,41 @@ void moveCursor(WINDOW *win, int &y, int &x) {
         if (y == 72) {
             return;
         }
-        wmove(win, x, y + 4);
         y += 4;
+        wmove(win, x, y);
     }
     if (c == KEY_LEFT) {
         if (y == 0) {
             return;
         }
-        wmove(win, x, y - 4);
         y -= 4;
+        wmove(win, x, y);
     }
     if (c == KEY_UP) {
         if (x == 0) {
             return;
         }
-        wmove(win, x - 2, y);
         x -= 2;
+        wmove(win, x, y);
     }
     if (c == KEY_DOWN) {
         if (x == 36) {
             return;
         }
-        wmove(win, x + 2, y);
         x += 2;
+        wmove(win, x, y);
+    }
+    if (c == 10) {
+        if (validate[x][y] && player) { // player 1
+            checkerboard[x][y] = 'O';
+            validate[x][y] = false;
+            player = false;
+        } else if (validate[x][y] && !player) { // player 2
+            checkerboard[x][y] = 'X';
+            validate[x][y] = false;
+            player = true;
+        }
+        printCheckerboard(win, checkerboard);
+        wmove(win, x, y);
     }
 }
